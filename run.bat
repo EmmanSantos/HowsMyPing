f60822f7476f48873da1235bf9b6bc1562b623bd
Ehhmann <jes.santosph@gmail.com>
2025-07-09 17:00:54 +0800

    remove bat files

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Activate the virtual environment
-call venv\Scripts\activate.bat
-
-REM Run the Python script
-c:/Programs/ping_tester/.venv/Scripts/python.exe c:/Programs/ping_tester/main.py
-
-REM Optional: pause so you can see output
-pause
